b49a4d3d5395bbed316888302f399c14877ed3d7
xiaowei-hu <xiaowei.hu@outlook.com>
2022-02-08 13:17:41 +0800

    update op.func args

--- examples/sleep.py
+++ examples/sleep.py
@@ -10,9 +10,9 @@ logging.basicConfig(level=logging.INFO)
 
 # 初始化资源
 op_manager = de.OpManager()
-op_manager.add(de.Op('sleep_3_op', lambda x, y: logging.info(
+op_manager.add(de.Op('sleep_3_op', lambda x, y, z: logging.info(
     'Sleep 3s. ' + str(time.sleep(3)))))
-op_manager.add(de.Op('sleep_5_op', lambda x, y: logging.info(
+op_manager.add(de.Op('sleep_5_op', lambda x, y, z: logging.info(
     'Sleep 5s. ' + str(time.sleep(5)))))
 serial_engine = de.Engine(1)
 parallel_engine = de.Engine()
@@ -20,12 +20,14 @@ parallel_engine = de.Engine()
 
 # 运行时
 tic = time.time()
+join_node = de.Node('join_node', op_manager.get('join_op'))
 sleep_node_a = de.Node('sleep_node_a', op_manager.get('sleep_5_op'))
 sleep_node_b = de.Node('sleep_node_b', op_manager.get('sleep_3_op'))
 sleep_node_c = de.Node('sleep_node_c', op_manager.get('sleep_3_op'))
 g = de.Graph()
-g.add_nodes_from([sleep_node_a, sleep_node_b, sleep_node_c])
-g.add_edges_from([(sleep_node_b.name, sleep_node_c.name)])
+g.add_nodes_from([join_node, sleep_node_a, sleep_node_b, sleep_node_c])
+g.add_edges_from([(sleep_node_a.name, join_node.name),
+                 (sleep_node_b.name, sleep_node_c.name)])
 g.froze()
 print('build graph time: %s' % str(time.time() - tic))
 
